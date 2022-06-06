d19bda7e4f198a9d7f740f3e75afc9a8b21fbce3
andyjohn23 <andrewnick906@gmail.com>
2022-06-06 11:29:59 +0300

    completed the monkey patch on the question method

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,13 +2,16 @@ require 'pry'
 
 class String
 
-  def sentence?
-    self.end_with?(".")
-  end
-  puts "Hi, my name is Sophie.".sentence?
-  # def question?
-
+  # def sentence?
+  #   self.end_with?(".")
   # end
+  # puts "Hi, my name is Sophie.".sentence?
+
+  def question?
+    self.end_with?("?")
+  end
+  puts "Hi, my name is Sophie?".question?
+
 
   # def exclamation?
 
